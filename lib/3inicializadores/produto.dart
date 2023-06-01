e081b8ca32e338bdf3602e6c7d34258f653a5900
Fabrício Suhet <fabriciosuhet@hotmail.com>
2023-06-01 17:13:07 -0300

    inicializadores

---
+++ lib/3inicializadores/produto.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: avoid_print
+
+class Produto {
+  final int _id;
+  final String nome;
+  final double _preco;
+
+  // Produto({
+  //   required this.id,
+  //   required this.preco,
+  //   required this.nome,
+  // });
+  Produto({
+    required int id,
+    required this.nome,
+    required double preco,
+  })  : _id = id,
+        _preco = preco{
+          print(_id);
+          print(preco);
+        }
+
+  factory Produto.fabrica({
+    required int id,
+    required String nome,
+    required double preco,
+  }) {
+    return Produto(id: id, nome: nome, preco: preco);
+  }
+}
