6b1d9fef1adc3ffeaac9bd940475cffbaefd416e
Fabrício Suhet <fabriciosuhet@hotmail.com>
2023-06-12 21:14:55 -0300

    operator methods

---
+++ lib/13operator_methods/operator_method1.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: avoid_print
+
+import 'package:dart_poo/13operator_methods/numeros.dart';
+
+void main () {
+
+  var num1 = Numero(10);
+  var num2 = Numero(20);
+
+  var totalSoma = num1 + num2;
+
+  print(totalSoma.i);
+
+  var totalSub = num1 - num2;
+  print(totalSub.i);
+
+}
