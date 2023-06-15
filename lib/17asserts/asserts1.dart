966271a9302585467e772d529ed50d16f0f2ddd9
Fabrício Suhet <fabriciosuhet@hotmail.com>
2023-06-15 19:19:27 -0300

    "asserts"

---
+++ lib/17asserts/asserts1.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: avoid_print
+
+import 'package:dart_poo/17asserts/cliente.dart';
+
+void main() {
+  var cli = Cliente(
+    // cpf: '123123',
+    nome: 'jkasdjkas',
+    razaoSocial: '123123',
+    cnpj: '123123',
+  );
+}
